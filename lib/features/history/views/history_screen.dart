33f6df3ab6ccc2bdb4c4dcca6e9014338027c217
김동규 <dgorzldd@gmail.com>
2024-07-11 00:35:41 +0900

    주문내역 페이지 퍼블리시

--- lib/features/history/views/history_screen.dart
+++ lib/features/history/views/history_screen.dart
@@ -1,3 +1,4 @@
+import 'package:angel_eats_test/features/history/views/widgets/history_list_item.dart';
 import 'package:flutter/material.dart';
 
 class HistoryScreen extends StatelessWidget {
@@ -8,9 +9,42 @@ class HistoryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("HistoryScreen"),
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                child: Text(
+                  "필터",
+                ),
+              ),
+              ListView.separated(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return const HistoryListItem(
+                    deliveryDate: "6. 30 (일)",
+                    deliveryState: "배달완료",
+                    storeName: "마포찜닭",
+                    storeMenu: "찜닭+날치알주볶밥+음료 세트 1개 28,600원",
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return Divider(
+                    color: colorScheme.primary.withOpacity(.2),
+                    thickness: 10,
+                  );
+                },
+                itemCount: 5,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/features/history/views/widgets/history_list_item.dart
@@ -0,0 +1,129 @@
+import 'package:angel_eats_test/constants/gaps.dart';
+import 'package:flutter/material.dart';
+
+class HistoryListItem extends StatelessWidget {
+  const HistoryListItem({
+    super.key,
+    required this.deliveryDate,
+    required this.deliveryState,
+    required this.storeName,
+    required this.storeMenu,
+  });
+
+  final String deliveryDate;
+  final String deliveryState;
+  final String storeName;
+  final String storeMenu;
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "$deliveryDate • $deliveryState",
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.black.withOpacity(.6),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 2,
+                  horizontal: 5,
+                ),
+                decoration: BoxDecoration(
+                  color: colorScheme.surface,
+                  borderRadius: const BorderRadius.all(Radius.circular(20)),
+                  border: Border.all(
+                    color: colorScheme.primary.withOpacity(.2),
+                  ),
+                ),
+                child: const Text(
+                  "주문상세",
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Gaps.v10,
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.2,
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: colorScheme.primary,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+              ),
+              Gaps.h10,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          storeName,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Gaps.h2,
+                        const Icon(
+                          Icons.arrow_forward_ios_rounded,
+                          size: 14,
+                        ),
+                      ],
+                    ),
+                    Text(
+                      storeMenu,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          Gaps.v10,
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: colorScheme.primary,
+                width: 1,
+              ),
+              borderRadius: BorderRadius.circular(5),
+            ),
+            padding: const EdgeInsets.symmetric(
+              vertical: 10,
+            ),
+            child: Text(
+              "같은 메뉴 담기",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: colorScheme.primary,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
